a3146c096be66e85ed4ff7b6d4d3461253d255ae
D-Joseph <danielrjoseph8@gmail.com>
2022-01-19 13:58:44 -0500

    Changed .evn var names and added comments

--- app.py
+++ app.py
@@ -4,13 +4,15 @@ from splitwise import Splitwise
 from sms import send
 
 # Splitwise Authorization
-s = Splitwise(os.getenv("KEY1"),os.getenv("KEY2"),api_key=os.getenv("API_KEY"))
+s = Splitwise(os.getenv("CONSUMER_KEY"),os.getenv("CONSUMER_SECRET"),api_key=os.getenv("API_KEY"))
 
 # Get a list of all groups, without the non-group expenses
 myGroups = s.getGroups()[1:]
 
 for i in myGroups:
+  # Cycle through groups and get each member
   members = i.getMembers()
   for j in members:
+    # For each member, see if they owe money and if they do run the send command in sms.py
     if(float(j.getBalances()[0].getAmount()) < 0):
       send(j.getFirstName(), j.getBalances()[0].getAmount()[1:], i.getName())
